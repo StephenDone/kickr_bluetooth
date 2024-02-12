8dc1cf8f0ec6a706ce3805b1273a88a270b16bfd
Stephen Done <stephenrdone@gmail.com>
2024-02-12 19:30:42 +0000

    Tweaked scanner for Windows compatibility.

--- kickr/scanner.py
+++ kickr/scanner.py
@@ -1,7 +1,7 @@
 import bleak
 
 def device_filter(d, ad):
-            found = d.name.startswith('KICKR BIKE') and ad.service_uuids is not None
+            found = d.name and d.name.startswith('KICKR BIKE') and ad.service_uuids is not None
             if(found):
                 print(f'Found KICKR with name "{ad.local_name}", advertising...')
                 for uuid in ad.service_uuids:
