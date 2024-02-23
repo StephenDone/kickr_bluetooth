1a3974e8d61f3d84c4bac60fc49dae735fa5f70a
Stephen Done <stephenrdone@gmail.com>
2024-02-23 17:28:04 +0000

    Added signed_16 and unsigned_32 functions.

--- convert.py
+++ convert.py
@@ -10,6 +10,16 @@ def ToHex(bytes):
 def unsigned_16(b1, b2):
     return b1 | b2 << 8
 
+def signed_16(b1, b2):
+    return sign_extend(unsigned_16(b1, b2), 16)
+
 def sign_extend(value, bits):
     sign_bit = 1 << (bits - 1)
     return (value & (sign_bit - 1)) - (value & sign_bit)
+
+def unsigned_32(b1, b2, b3, b4):
+    return b1 \
+        | (b2 << 8) \
+        | (b3 << 16) \
+        | (b4 << 24) 
+    
