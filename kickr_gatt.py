2c05dd76a3b502dfe1d348396b7eb0ed18bd5377
Stephen Done <stephenrdone@gmail.com>
2024-02-23 19:53:04 +0000

    Incorporated user data & cycling power service into main script.

--- kickr_gatt.py
+++ kickr_gatt.py
@@ -1,17 +1,15 @@
 import asyncio
-import bleak
 from bleak import BleakClient
-from bleak import BleakScanner
-from bleak.backends.characteristic import BleakGATTCharacteristic
 
 import kickr.uuids
 import kickr.scanner
-import kickr.devinfo
+import kickr.devinfo as devinfo
 import kickr.tilt as tilt
 import kickr.chosen_gear as chosen_gear
 import kickr.buttons as buttons
-
-from convert import ToString, ToHex
+import kickr.user_data as user_data
+import kickr.cycling_power as cycling_power
+import kickr.cycling_power_extension as power_extension
 
 ShowCharacteristicDescriptors = False
 
@@ -33,21 +31,33 @@ async def main():
         
         kickr.uuids.print_uuids(client)
 
-        await kickr.devinfo.read(client)
+        await devinfo.read(client)
+
+        await user_data.read(client)
+
+        await power_extension.start_notify(client)
+        await power_extension.power_extension_tests(client)
+        await power_extension.stop_notify(client)
+
+        await cycling_power.cycling_power_tests(client)
+        await cycling_power.start_notify(client)
+        await asyncio.sleep(1)
+        await cycling_power.stop_notify(client)
 
         await tilt.start_notify(client)
         await tilt.read_tests(client)
         await tilt.stop_notify(client)
 
         await chosen_gear.start_notify(client)
-        await asyncio.sleep(10)
+        await asyncio.sleep(3)
         await chosen_gear.stop_notify(client)
 
         await buttons.start_notify(client)
-        await asyncio.sleep(10)
+        await asyncio.sleep(3)
         await buttons.stop_notify(client)
 
     finally:
         await client.disconnect()
 
 asyncio.run(main())
+
